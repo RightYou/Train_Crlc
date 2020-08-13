a2290ecfaa6fff5a547540d35effeb02077b944a
Wu Joel <2210753946@qq.com>
2020-08-13 21:08:38 +0800

    16th commit

---
+++ VDSR_WARN_C2.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+
+
+def resblock(temp_tensor, convId):
+    conv_secondID = 0
+    skip_tensor = temp_tensor
+
+    conv_w = tf.get_variable("conv_%02d_%02d_w" % (convId, conv_secondID), [1, 1, 12, 32],
+                             initializer=tf.contrib.layers.xavier_initializer())
+    conv_b = tf.get_variable("conv_%02d_%02d_b" % (convId, conv_secondID), [32], initializer=tf.constant_initializer(0))
+    tf.add_to_collection(tf.GraphKeys.WEIGHTS, tf.contrib.layers.l2_regularizer(1.)(conv_w))
+    out_tensor = tf.nn.relu(
+        tf.nn.bias_add(tf.nn.conv2d(temp_tensor, conv_w, strides=[1, 1, 1, 1], padding='SAME'), conv_b))
+    conv_secondID += 1
+
+    conv_w = tf.get_variable("conv_%02d_%02d_w" % (convId, conv_secondID), [1, 1, 32, 8],
+                             initializer=tf.contrib.layers.xavier_initializer())
+    conv_b = tf.get_variable("conv_%02d_%02d_b" % (convId, conv_secondID), [8], initializer=tf.constant_initializer(0))
+    tf.add_to_collection(tf.GraphKeys.WEIGHTS, tf.contrib.layers.l2_regularizer(1.)(conv_w))
+    out_tensor = tf.nn.bias_add(tf.nn.conv2d(out_tensor, conv_w, strides=[1, 1, 1, 1], padding='SAME'), conv_b)
+    conv_secondID += 1
+
+    conv_w = tf.get_variable("conv_%02d_%02d_w" % (convId, conv_secondID), [3, 3, 8, 12],
+                             initializer=tf.contrib.layers.xavier_initializer())
+    conv_b = tf.get_variable("conv_%02d_%02d_b" % (convId, conv_secondID), [32], initializer=tf.constant_initializer(0))
+    tf.add_to_collection(tf.GraphKeys.WEIGHTS, tf.contrib.layers.l2_regularizer(1.)(conv_w))
+    out_tensor = tf.nn.bias_add(tf.nn.conv2d(out_tensor, conv_w, strides=[1, 1, 1, 1], padding='SAME'), conv_b)
+    conv_secondID += 1
+
+    # skip + out_tensor
+    out_tensor = tf.add(skip_tensor, out_tensor)
+    return out_tensor
+
+
+def model(input_tensor):
+    convId = 0
+    conv_00_w = tf.get_variable("conv_%02d_w" % (convId), [3, 3, 1, 12],
+                                initializer=tf.contrib.layers.xavier_initializer())
+    conv_00_b = tf.get_variable("conv_%02d_b" % (convId), [12], initializer=tf.constant_initializer(0))
+    tf.add_to_collection(tf.GraphKeys.WEIGHTS, tf.contrib.layers.l2_regularizer(1.)(conv_00_w))
+    tensor = tf.nn.bias_add(tf.nn.conv2d(input_tensor, conv_00_w, strides=[1, 1, 1, 1], padding='SAME'), conv_00_b)
+    convId += 1
+
+    # Residual Block x 3
+    for i in range(3):
+        tensor = resblock(tensor, convId)
+        convId += 1
+
+    conv_w = tf.get_variable("conv_%02d_w" % (convId), [3, 3, 12, 2],
+                             initializer=tf.contrib.layers.xavier_initializer())
+    conv_b = tf.get_variable("conv_%02d_b" % (convId), [2], initializer=tf.constant_initializer(0))
+    tf.add_to_collection(tf.GraphKeys.WEIGHTS, tf.contrib.layers.l2_regularizer(1.)(conv_w))
+    tensor = tf.nn.bias_add(tf.nn.conv2d(tensor, conv_w, strides=[1, 1, 1, 1], padding='SAME'), conv_b)
+
+    tensor = tf.add(tensor, input_tensor)
+
+    return tensor
--- cnn_CRLC_v2_Note.py
+++ cnn_CRLC_v2_Note.py
@@ -1,27 +1,81 @@
 import sys
 import numpy.linalg as lg
-from RVDSR_WARN_2 import model as CRLC
+import matplotlib.pyplot as plt  # 用于绘制A的分布图，便于量化
 from UTILS import *
+from RVDSR_WARN_2 import model as model
 
 if not hasattr(sys, 'argv'):  # 如果没有命令，就把命令置为空，用处未知
     sys.argv = ['']
-
 np.set_printoptions(threshold=np.inf)  # 用于数组完全显示
-tplt1 = "{0:^30}\t{1:^10}\t{2:^10}\t{3:^10}\t{4:^10}"  # 用于格式化输出
-tplt = "{0:^30}\t{1:^10}\t{2:^10}"  # 用于格式化输出
 
+format_pt = "{0:^30}\t{1:^10}\t{2:^10}"  # 用于格式化输出
+
+# init函数使用，模型权重文件路径，通过字典的方式便于获取
 model_set = {
+    "QP7~16_C2": r"E:\best\CRLCv24_real_I_QP7~16_C2\CRLCv24_real_I_QP7~16_C2_20040101_097_-0.49.ckpt",
+    "QP17~26_C2": r"E:\best\CRLCv24_real_I_QP17~26_C2\CRLCv24_real_I_QP17~26_C2_20040201_107_-1.42.ckpt",
+    "QP27~36_C2": r"E:\best\CRLCv24_real_I_QP27~36_C2\CRLCv24_real_I_QP27~36_C2_20040701_165_-14.12.ckpt",
+    "QP37~46_C2": r"E:\best\CRLCv24_real_I_QP37~46_C2\CRLCv24_real_I_QP37~46_C2_20040301_178_-10.60.ckpt",
+    "QP47~56_C2": r"E:\best\CRLCv24_real_I_QP47~56_C2\CRLCv24_real_I_QP47~56_C2_20040301_194_-21.95.ckpt",
+    "QP57~66_C2": r"E:\best\CRLCv24_real_I_QP57~66_C2\CRLCv24_real_I_QP57~66_C2_20040301_190_-39.37.ckpt",
+    "test": r""
+}
 
+# predict函数使用，量化范围集合
+range_set = {
+    "range8": {
+        "QP7~16_C2": (512, 1, -5),
+        "QP17~26_C2": (256, 0, -1),
+        "QP27~36_C2": (512, -7, -7),
+        "QP37~46_C2": (256, -5, -7),
+        "QP47~56_C2": (256, -8, -4),
+        "QP57~64_C2": (128, -6, -7)
+    },
+    "range16": {
+        "QP7~16_C2": (1024, 0, -7),
+        "QP17~26_C2": (512, 2, -3),
+        "QP27~36_C2": (1024, -13, -15),
+        "QP37~46_C2": (512, -13, -13),
+        "QP47~56_C2": (512, -15, -10),
+        "QP57~64_C2": (256, -9, -10)
+    },
+    "range32": {
+        "QP7~16_C2": (2048, 5, -24),
+        "QP17~26_C2": (1024, 2, -3),
+        "QP27~36_C2": (2048, -23, -28),
+        "QP37~46_C2": (1024, -23, -23),
+        "QP47~56_C2": (1024, -30, -10),
+        "QP57~64_C2": (512, -16, -20)
+    },
+    "range64": {
+        "QP7~16_C2": (2048, 15, -30),
+        "QP17~26_C2": (2048, 10, -10),
+        "QP27~36_C2": (4096, -45, -60),
+        "QP37~46_C2": (2048, -40, -45),
+        "QP47~56_C2": (2048, -60, -30),
+        "QP57~64_C2": (1024, -40, -40)
+    },
+    "range0": {  # 用于调参
+        "QP7~16_C2": (128, 15, -30),
+        "QP17~26_C2": (128, 10, -10),
+        "QP27~36_C2": (128, -45, -60),
+        "QP37~46_C2": (128, -40, -45),
+        "QP47~56_C2": (128, -60, -30),
+        "QP57~64_C2": (128, -40, -40)
+    }
 }
 
 # 设置tf.Session的运算方式，同时设置了GPU的使用
 config = tf.ConfigProto(allow_soft_placement=True)  # 当运行设备不满足要求时，会自动分配GPU或者CPU。
-config.gpu_options.per_process_gpu_memory_fraction = 1  # GPU内存使用最大比例，这里设置为1，就会限制共享内存的使用，所以可以注释
+config.gpu_options.per_process_gpu_memory_fraction = 0.8  # GPU内存使用最大比例，这里设置为1，就会限制共享内存的使用，所以可以注释
 config.gpu_options.allow_growth = True  # 运行GPU内存自增长
 
 # 定义用来调用模型的全局变量
-global mCRLC
-global cnn7, cnn17, cnn27, cnn37, cnn47, cnn57
+global CNN_Model
+
+# 用于统计A0和A1的值的分布
+a0_num = dict()
+a1_num = dict()
 
 
 # 准备测试数据
@@ -56,7 +110,6 @@ def prepare_test_data(fileOrDir):
 
     ##The input is two directories, including ground truth.
     elif len(fileOrDir) == 2:
-
         fileName_list = load_file_list(fileOrDir[0])
         test_list = get_train_list(load_file_list(fileOrDir[0]), load_file_list(fileOrDir[1]))
         for pair in test_list:
@@ -145,9 +198,6 @@ class Predict:
                 print(modelpath)
 
     def predict(self, fileOrDirX, fileOrDirY):
-        imgX = None
-        imgY = None
-
         if (isinstance(fileOrDirX, str)):
             original_ycbcr0, gt_y, fileName_list = prepare_test_data(fileOrDirX)
             original_ycbcr1, gt_y, fileName_list = prepare_test_data(fileOrDirY)
@@ -155,7 +205,6 @@ class Predict:
             imgY = original_ycbcr1[0][0]
 
         elif type(fileOrDirX) is np.ndarray:
-
             imgX = fileOrDirX
             imgY = fileOrDirY
 
@@ -181,70 +230,46 @@ class Predict:
                 return out
 
 
-def init(sliceType, QP):
-    print("_______________init_________________")
-
-    global cnn7, cnn17, cnn27, cnn37, cnn47, cnn57
-    cnn7 = Predict(CRLC, model_set["CRLCv24_I_QP7~16_C2"])
-    cnn17 = Predict(CRLC, model_set["CRLCv24_I_QP17~26_C2"])
-    cnn27 = Predict(CRLC, model_set["CRLCv24_I_QP27~36_C2"])
-    cnn37 = Predict(CRLC, model_set["CRLCv24_I_QP37~46_C2"])
-    cnn47 = Predict(CRLC, model_set["CRLCv24_I_QP47~56_C2"])
-    cnn57 = Predict(CRLC, model_set["CRLCv24_I_QP57~66_C2"])
-
-    # global mCRLC
-    # QP = QP / 4 # aom传过来的是base_qpindex是0-255的，而参数配置的QP是0-64的，所以要除4
-    # if QP ==32:
-    #     mCRLC = Predict(CRLC, model_set['CRLC4_I_QP32_C2_V6'])
-    # elif QP ==43:
-    #     mCRLC = Predict(CRLC, model_set['CRLC4_I_QP43_C2_V6'])
-    # elif QP==53:
-    #     mCRLC =Predict(CRLC,model_set['CRLC4_I_QP53_C2_V6_db'])   #CRLC4_I_QP53_C2_V6
-    # elif QP >= 63:
-    #     mCRLC = Predict(CRLC, model_set['CRLC4_I_QP63_C2_V6'])
-    # mCRLC = Predict(CRLC, model_set['CRLCv6_I_QP47-55_C2_202103_CNN1'])
+def init(frame_type, qp):
+    print("----------Start to initialize the model----------")
+    global CNN_Model
+    qp = qp / 4  # aom传过来的是qp是0-255的，而参数配置的qp是0-64的，所以要除4
+    # model: 函数本身也是对象，所以可以将函数作为参数传入另一函数并进行调用
+    if qp < 17:
+        CNN_Model = Predict(model, model_set["QP7~16_C2"])
+    elif 17 <= qp < 27:
+        CNN_Model = Predict(model, model_set["QP17~26_C2"])
+    elif 27 <= qp < 37:
+        CNN_Model = Predict(model, model_set["QP27~36_C2"])
+    elif 37 <= qp < 47:
+        CNN_Model = Predict(model, model_set["QP37~46_C2"])
+    elif 47 <= qp < 57:
+        CNN_Model = Predict(model, model_set["QP47~56_C2"])
+    else:
+        CNN_Model = Predict(model, model_set["QP57~66_C2"])
+    print("-------Successfully initialized the model-------")
 
 
 def predict(dgr, src, qp, block_size=256):
     qp = qp / 4
-    arrange = 16
-    # 根据不同QP使用不同的量化范围
+    print("当前QP为：", qp)
+    A_range = 16
+    # 根据qp不同，使用不同量化范围
     if qp < 17:
-        scale = 1024
-        A0_min, A1_min = 0, -7
+        scale, A0_min, A1_min = range_set["range" + str(A_range)]["QP7~16_C2"]
     elif 17 <= qp < 27:
-        scale = 512
-        A0_min, A1_min = 2, -3
+        scale, A0_min, A1_min = range_set["range" + str(A_range)]["QP17~26_C2"]
     elif 27 <= qp < 37:
-        scale = 1024
-        A0_min, A1_min = -9, -15
+        scale, A0_min, A1_min = range_set["range" + str(A_range)]["QP27~36_C2"]
     elif 37 <= qp < 47:
-        scale = 512
-        A0_min, A1_min = -13, -13
+        scale, A0_min, A1_min = range_set["range" + str(A_range)]["QP37~46_C2"]
     elif 47 <= qp < 57:
-        scale = 512
-        A0_min, A1_min = -15, -10
+        scale, A0_min, A1_min = range_set["range" + str(A_range)]["QP47~56_C2"]
     else:
-        scale = 256
-        A0_min, A1_min = -9, -10
+        scale, A0_min, A1_min = range_set["range" + str(A_range)]["QP57~64_C2"]
 
-    '''
-    if QP < 17:
-        R = cnn7.predict(dgr, src)
-    elif 17 <= QP < 27:
-        R = cnn17.predict(dgr, src)
-    elif 27 <= QP < 37:
-        R = cnn27.predict(dgr, src)
-    elif 37 <= QP < 47:
-        R = cnn37.predict(dgr, src)
-    elif 47 <= QP < 57:
-        R = cnn47.predict(dgr, src)
-    else:
-        R = cnn57.predict(dgr, src)
-    '''
-
-    global mCRLC
-    R = mCRLC.predict(dgr, src)
+    global CNN_Model
+    R = CNN_Model.predict(dgr, src)
 
     hei = np.shape(dgr)[0]
     wid = np.shape(dgr)[1]
@@ -263,49 +288,72 @@ def predict(dgr, src, qp, block_size=256):
                 start_row = i * block_size
                 end_row = (i + 1) * block_size
             if j == cols - 1:
-                start_clow = wid - block_size
-                end_clow = wid
+                start_col = wid - block_size
+                end_col = wid
             else:
-                start_clow = j * block_size
-                end_clow = (j + 1) * block_size
+                start_col = j * block_size
+                end_col = (j + 1) * block_size
             if hei < block_size:
                 start_row = 0
                 end_row = hei
             if wid < block_size:
-                start_clow = 0
-                end_clow = wid
-            sub_dgr = dgr[start_row:end_row, start_clow:end_clow]
-            sub_src = src[start_row:end_row, start_clow:end_clow]
+                start_col = 0
+                end_col = wid
+
+            # print(start_row, end_row, start_col, end_col)  # 对图像通过切片的方式进行分块处理
+            sub_dgr = dgr[start_row:end_row, start_col:end_col]
+            sub_src = src[start_row:end_row, start_col:end_col]
             sub_r = (sub_src - sub_dgr).flatten()
-            sub_R = np.reshape(R[start_row:end_row, start_clow:end_clow, :],
-                               ((end_clow - start_clow) * (end_row - start_row), np.shape(R)[-1]))
-            A = lg.inv(sub_R.T.dot(sub_R)).dot(sub_R.T).dot(sub_r) * scale
-            # 码率和画质的平衡
-            # 对A量化
+            sub_R = np.reshape(R[start_row:end_row, start_col:end_col, :],
+                               ((end_col - start_col) * (end_row - start_row), np.shape(R)[-1]))
+            A = np.linalg.inv(sub_R.T.dot(sub_R)).dot(sub_R.T).dot(sub_r) * scale
+
+            # 对A量化，码率和画质的平衡
             # A = np.around(A)
             # A[0] = np.clip(A[0], A0_min, A0_min+arrange-1)
             # A[1] = np.clip(A[1], A1_min, A1_min+arrange-1)
-
             # print(A)
 
-            sub_rec = sub_dgr + np.sum(np.multiply(R[start_row:end_row, start_clow:end_clow, :], A / scale), axis=2)
-            A_list.append(A.astype('int'))
+            # 统计A的分布
+            # if A[0] not in a0_num:
+            #     a0_num[A[0]] = 1
+            # else:
+            #     a0_num[A[0]] += 1
+            #
+            # if A[1] not in a1_num:
+            #     a1_num[A[1]] = 1
+            # else:
+            #     a1_num[A[1]] += 1
 
-            rec[start_row:end_row, start_clow:end_clow] = sub_rec
+            # 通过A的值组合通道还原重建图像
+            sub_rec = sub_dgr + np.sum(np.multiply(R[start_row:end_row, start_col:end_col, :], A / scale), axis=2)
+            rec[start_row:end_row, start_col:end_col] = sub_rec
+
+            A_list.append(A.astype('int'))
 
     A_list = np.array(A_list).flatten()
     A_list = np.around(A_list)
     A_list = A_list.astype('int')
     A_list = A_list.tolist()
+    # print(A_list,flush=True)
 
     rec = np.around(rec)
     rec = np.clip(rec, 0, 255)
     rec = rec.astype('int')
     rec = rec.tolist()
+    # print("psnr：", psnr(src, rec))
+    # show_img(rec)  # 显示重建图像
     return rec, A_list
 
 
+def show_img(img_np):
+    tem = np.asarray(img_np, dtype='uint8')
+    tem = Image.fromarray(tem, 'L')
+    tem.show()
+
+
 def test_all_ckpt(modelPath):
+    global CNN_Model
     low_img = r"F:\0wzy_Data\test_set\QP53"
     high_img = r"F:\0wzy_Data\test_set\label"
     original_ycbcr, gt_y, file_name_list = prepare_test_data([low_img, high_img])  # 加载验证集及其标签
@@ -316,24 +364,54 @@ def test_all_ckpt(modelPath):
 
     max_ckpt = 0  # 最好的模型
     max_ckpt_psnr = 0  # 最大的峰值信噪比
-    global mCRLC
     for ckpt in ckpt_files:
         epoch = int(ckpt.split('.')[0].split('_')[-2])  # 获取这个模型是第多少个epoch训练出来的
-
         # if epoch != 373:
         #     continue
 
-        mCRLC = Predict(CRLC, os.path.join(modelPath, ckpt))  # mCRLC是全局变量，所以参数已经直接传进predict了
+        CNN_Model = Predict(model, os.path.join(modelPath, ckpt))  # CNN_Model是全局变量，已经直接传进predict了
         sum_img_psnr = 0
         img_index = [14, 17, 4, 2, 7, 10, 12, 3, 0, 13, 16, 5, 6, 1, 15, 8, 9, 11]  # 手动乱序，提高效果
         for i in img_index:
             imgY = original_ycbcr[i][0]
             gtY = gt_y[i] if gt_y else 0
+
+            # 不知道干什么的代码
+            # print(np.shape(imgY), np.shape(gtY))
+            # showImg(denormalize(np.reshape(original_ycbcr[i][0], [480, 832])))
+            # showImg(np.reshape(gtY, [480, 832]))
+            # print(imgY.shape)
+            #
+            # block_size = 64
+            # padding_size = 8
+            # sub_imgs = zip(divide_img(imgY, block_size, padding_size),
+            #                divide_img((normalize(gtY)), block_size, padding_size))
+            # recs = []
+            # for lowY, gY in sub_imgs:
+            #     # print(type(predictor.predict(lowY, gY)))
+            #     recs.append(predictor.predict(lowY, gY)[0])
+            # rec = compose_img(imgY, recs, block_size, padding_size)
+            # # print(psnr(np.reshape(denormalize(imgY), np.shape(rec)), np.reshape(gtY, np.shape(rec))))
+            # cur_img_psnr = psnr(rec, np.reshape(gtY, np.shape(rec)))
+
             rec, _ = predict(denormalize(imgY)[0, :, :, 0].tolist(), gtY[0, :, :, 0].tolist(), 212, 256)
             cur_img_psnr = psnr(rec, np.reshape(gtY, np.shape(rec)))
-            sum_img_psnr = cur_img_psnr + sum_img_psnr  # 对图片psnr求和
-            print(tplt.format(os.path.basename(file_name_list[i]), cur_img_psnr,
-                              psnr(denormalize(np.reshape(imgY, np.shape(rec))), np.reshape(gtY, np.shape(rec)))))
+
+            # 不知道干什么的代码X2
+            # print(psnr(denormalize(np.reshape(imgY[:, :64, :64, :], [np.shape(imgY[:, :64, :64, :])[1],
+            #                                                          np.shape(imgY[:, :64, :64, :])[2]])),
+            #            np.reshape(gtY[:, :64, :64, :],
+            #                       [np.shape(imgY[:, :64, :64, :])[1], np.shape(imgY[:, :64, :64, :])[2]])))
+            # showImg(np.reshape(gtY[:, :64, :64, :], np.shape(rec)))
+            # cur_psnr[cnnTime] = psnr(rec, np.reshape(gtY[:, :64, :64, :], np.shape(rec)))
+            #
+            # print(psnr(denormalize(np.reshape(imgY, [np.shape(imgY)[1], np.shape(imgY)[2]])),
+            #            np.reshape(gtY, [np.shape(imgY)[1], np.shape(imgY)[2]])))
+
+            # 对图片psnr求和
+            sum_img_psnr = cur_img_psnr + sum_img_psnr
+            print(format_pt.format(os.path.basename(file_name_list[i]), cur_img_psnr,
+                                   psnr(denormalize(np.reshape(imgY, np.shape(rec))), np.reshape(gtY, np.shape(rec)))))
 
         cur_ckpt_psnr = sum_img_psnr / total_img  # 模型的psnr等于图片psnr的均值
         if cur_ckpt_psnr > max_ckpt_psnr:
